7c929c9ab1e1c7c92218488e5147c22b57b47e64
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-05-07 14:45:51 -0700

    db connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+// const db =
+//   "mongodb+srv://hamzanayab:hamza@mernblog.zqav0wp.mongodb.net/mern-blog";
 
-app.listen(port, ()=>{
-    console.log(`running on port: ${port}`);
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log(`running on port: ${port}`);
 });
