627ab944f946967b1f2d66b576c4afdc7337e0fe
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-05-08 12:24:50 -0700

    create bcrypt

--- api/controller/AuthController.js
+++ api/controller/AuthController.js
@@ -1,5 +1,32 @@
 import { query } from "express";
+import user from "../models/usermodel.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { email, username, password } = req.body;
+  console.log(email + username + password);
+  if (
+    !email ||
+    !username ||
+    !password ||
+    email === "" ||
+    username === "" ||
+    password == ""
+  ) {
+    return res.status(400).json({ message: "All fields are Required" });
+  }
+  const hashed =  bcryptjs.hashSync(password,10);
+
+  const newUser = new user({
+    username,
+    email,
+    password: hashed,
+  });
+  try {
+    await newUser.save();
+    console.log("done");
+    res.status(200).json({ message: "Signup Successful" });
+  } catch (err) {
+    res.status(420).json({ message: err });
+  }
 };
