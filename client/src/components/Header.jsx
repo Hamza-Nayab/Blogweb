8fe0c0b3bd4a8dde796b90a7d6c50e8a7e5b4cd4
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-05-24 14:01:11 -0700

    dark Mode done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,12 +9,15 @@ import {
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice.js";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const {theme} = useSelector((state) => state.theme)
 
   return (
     <Navbar className="border-b-2">
@@ -39,8 +42,17 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color={"gray"} pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color={"gray"}
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          { theme === "light" ? (
+            <FaMoon />
+          ) : (
+            <FaSun />
+          )}
         </Button>
         {currentUser ? (
           <>
@@ -48,17 +60,14 @@ export default function Header() {
               arrowIcon={false}
               inline
               label={
-                <Avatar
-                  rounded
-                  alt="user"
-                  img={currentUser.profilePicture}
-                />
+                <Avatar rounded alt="user" img={currentUser.profilePicture} />
               }
             >
               <Dropdown.Header>
                 <span className="block text-sm">{currentUser.username}</span>
-                <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-
+                <span className="block text-sm font-medium truncate">
+                  {currentUser.email}
+                </span>
               </Dropdown.Header>
               <Link to={"/dashboard?tab=profile"}>
                 <Dropdown.Item>Profile</Dropdown.Item>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,15 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import themeReducer from "./theme/themeSlice.js"
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { version } from "react";
 import persistStore from "redux-persist/es/persistStore";
+import { theme } from "flowbite-react";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
